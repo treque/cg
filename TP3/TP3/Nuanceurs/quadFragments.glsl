8bfbd8e12fd992884e8c0a28faa248c9c86be671
Huyen Trang Dinh <tran9dinh@gmail.com>
2020-11-11 21:07:10 -0500

    cleanup pcq ca me tente

--- TP3/TP3/Nuanceurs/quadFragments.glsl
+++
@@ -1,9 +0,0 @@
-#version 430 core
-uniform sampler2D colorMap;
-in vec3 fragTexCoord;
-out vec4 color;
-
-void main(void)
-{
-    color = texture(colorMap, (1 + fragTexCoord.xy) / 2.0);
-}
--- TP3/TP3/TP3/Couleur.cpp
+++
@@ -1,6 +0,0 @@
-#include "Couleur.h"
-
-/// Constante de couleur NOIRE
-const CCouleur CCouleur::NOIR = CCouleur(0.0f);
-/// Constante de couleur BLANCHE
-const CCouleur CCouleur::BLANC = CCouleur(1.0f);
